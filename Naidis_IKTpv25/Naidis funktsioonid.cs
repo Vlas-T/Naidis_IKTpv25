24132fb307fbcf08e9d66a5a589fe57cb8786cdd
savva <smirnyaginsavva@gmail.com>
2026-02-18 13:07:11 +0200

    2

--- Naidis_IKTpv25/Naidis funktsioonid.cs
+++ Naidis_IKTpv25/Naidis funktsioonid.cs
@@ -6,35 +6,35 @@ namespace Naidis_IKTpv25
 {
     public class naidis_funktsioonid
     {
-        //    public static float Summa(float arv1, float arv2)
-        //    {
-        //        return arv1 + arv2;
-        //    }
-        //    public static string Kuu_nimetus(int kuu_nr)
-        //    {
-        //        string kuu = "";
-        //        switch (kuu_nr)
-        //        {
-        //            case 1: kuu = "Jaanuar"; break;
-        //            case 2: kuu = "Veebruar"; break;
-        //            case 3: kuu = "Märts"; break;
-        //            case 4: kuu = "Aprill"; break;
-        //            case 5: kuu = "Mai"; break;
-        //            case 6: kuu = "Juuni"; break;
-        //            case 7: kuu = "Juuli"; break;
-        //            case 8: kuu = "August"; break;
-        //            case 9: kuu = "September"; break;
-        //            case 10: kuu = "Oktoober"; break;
-        //            case 11: kuu = "Novemberr"; break;
-        //            case 12: kuu = "Detsember"; break;
+        /*    public static float Summa(float arv1, float arv2)
+            {
+                return arv1 + arv2;
+            }
+            public static string Kuu_nimetus(int kuu_nr)
+            {
+                string kuu = "";
+                switch (kuu_nr)
+                {
+                    case 1: kuu = "Jaanuar"; break;
+                    case 2: kuu = "Veebruar"; break;
+                    case 3: kuu = "Märts"; break;
+                    case 4: kuu = "Aprill"; break;
+                    case 5: kuu = "Mai"; break;
+                    case 6: kuu = "Juuni"; break;
+                    case 7: kuu = "Juuli"; break;
+                    case 8: kuu = "August"; break;
+                    case 9: kuu = "September"; break;
+                    case 10: kuu = "Oktoober"; break;
+                    case 11: kuu = "Novemberr"; break;
+                    case 12: kuu = "Detsember"; break;
 
-        //            default:
-        //                kuu = "???";
-        //                break;
-        //        }
-        //        return kuu;
-        //    }
-
+                    default:
+                        kuu = "???";
+                        break;
+                }
+                return kuu;
+            }
+        */
         public static string Hooaeg(int kuu_nr)
         {
             string hoo = "";
